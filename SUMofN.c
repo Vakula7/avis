3d5a8802259a61ec8f405f2a2a4c348f11a62ef4
sivapraveentsp <sivatspds@gmail.com>
2016-07-22 14:50:12 +0530

    Create SUMofN.c

---
+++ SUMofN.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	printf("\nEnter a number");
+	scanf("%d",&no);
+	for(int i=(n-1);i>=0;i--)
+	no+=i;
+	printf("\n%d",no);
+	return 0;
+}
